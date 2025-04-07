8c87b9dab6e9647552aa90abe6f9b647808283fd
Manu @StuFFmc Carrasco Molina <m+git@carrascomolina.com>
2025-04-07 23:46:12 +0200

    Syncing selection from Sidebar with content of SplitView

--- App.swift
+++ App.swift
@@ -1,41 +1,49 @@
 import SwiftUI
 
-enum Screen: String, RawRepresentable {
+extension [Screen]: @retroactive RawRepresentable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+            let result = try? JSONDecoder().decode([Screen].self, from: data)
+        else { return nil }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+            let result = String(data: data, encoding: .utf8)
+        else { return "[]" }
+        return result
+    }
+}
+
+enum Screen: String, Codable, Hashable, CaseIterable, Identifiable {
     case home
-    case detail
+    case detail // (id: Int)
+
+    var id: String { rawValue }
 }
 
 @main
 struct PathRestore: App {
-    @AppStorage("path") private var appNavigationPath: String?
-
-    var appNavigation: [Screen] {
-        get {
-            if appNavigationPath == nil || appNavigationPath!.isEmpty {
-                return []
-            }
-            return (appNavigationPath ?? String(Screen.home.rawValue)).split(separator: ".")
-                .compactMap { Screen(rawValue: String($0)) }
-        }
-    }
+    @AppStorage("path") private var path = [Screen]()
+    @AppStorage("selection") private var selection: Screen?
 
     var body: some Scene {
         WindowGroup {
             NavigationSplitView {
-                Text("Menu")
-            } detail: {
-                NavigationStack(
-                    path: Binding(
-                        get: { appNavigation },
-                        set: { newValue in
-                            appNavigationPath = newValue.map(\.rawValue).joined(separator: ".")
-                        })
-                ) {
-                    NavigationLink(value: Screen.detail) {
-                        Text("Next")
+                List(Screen.allCases) { screen in
+                    Button(screen.rawValue.capitalized) {
+                        selection = screen
+                        path = []
                     }
-                    .navigationDestination(for: Screen.self) { _ in
-                        Text("Detail")
+                    .buttonStyle(.plain)
+                    .foregroundStyle(screen == selection ? .primary : .secondary)
+                }
+            } detail: {
+                NavigationStack(path: $path) {
+                    NavigationLink(selection?.rawValue ?? "", value: selection)
+                    .navigationDestination(for: Screen.self) {
+                        Text($0.rawValue.uppercased())
                     }
                 }
             }
